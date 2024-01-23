abbd1ae3397928265a58100d68866f750566c978
Álvaro Silva Garcia <alvaro_s.g@hotmail.com>
2024-01-23 14:42:35 -0300

    chore: configurar projeto base com Vite + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
