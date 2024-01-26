d53184c12b7706d0c14b644e4bff1833a8e98387
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-01-26 03:42:14 -0300

    feat: add edit-menu component

---
+++ src/components/EditMenu/index.tsx
@@ -0,0 +1,42 @@
+import { Edit } from "@mui/icons-material"
+import { IconButton, Menu, MenuItem } from "@mui/material"
+import { useState } from "react";
+
+function EditMenu() {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <IconButton
+                id="edit-button"
+                aria-controls={open ? 'edit-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <Edit />
+            </IconButton>
+            <Menu
+                id="edit-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'edit-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Editar</MenuItem>
+                <MenuItem onClick={handleClose}>Excluir</MenuItem>
+            </Menu>
+        </>
+    )
+}
+
+export default EditMenu
