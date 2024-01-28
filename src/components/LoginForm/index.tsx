5f5bdfea83f9e6c6e37278f3c13ff27306ff4106
“vinicioscst” <dev.vinicios@gmail.com>
2024-01-28 20:14:58 -0300

    style: fixed components style

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -32,7 +32,7 @@ export default function LoginForm() {
 
   return (
     <form
-      className="flex flex-col gap-2 w-full max-w-[32.3125rem] p-4"
+      className="flex flex-col gap-4 w-full max-w-[32.3125rem] py-4"
       onSubmit={handleSubmit(onSubmit)}
       noValidate
     >
