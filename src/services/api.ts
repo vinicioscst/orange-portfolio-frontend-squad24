63cc5b1cbc1dc8fafd406a8253cfc239e8003fd7
“vinicioscst” <dev.vinicios@gmail.com>
2024-02-01 22:09:16 -0300

    fix: changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://hackathon-squad24.onrender.com",
+  baseURL: "https://deploy-backend-orange-juice.onrender.com/",
   timeout: 10000,
 });
