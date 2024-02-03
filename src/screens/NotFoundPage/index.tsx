f949510c3a2d7d475588e0e4c0f2c882d0d3c8e1
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-02-02 22:53:21 -0300

    feat: page notFound

--- src/screens/NotFoundPage/index.tsx
+++ src/screens/NotFoundPage/index.tsx
@@ -1,5 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material"
-import { Container } from "../../components/Container"
+import { Box, Grid, Typography, useTheme } from "@mui/material"
 import NotFound from "../../assets/notFound.svg";
 import Button from "../../components/Button";
 import { Link } from "react-router-dom";
@@ -7,30 +6,57 @@ import { Link } from "react-router-dom";
 type Props = {}
 
 function NotFoundPage({ }: Props) {
+    const theme = useTheme()
     return (
         <Box
             sx={{
                 background: 'linear-gradient(to top, #444466, #222244)',
-                height: '100vh'
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '2rem'
             }}
         >
-            <Grid>
-                <Typography>
-                    Ooops...
-                </Typography>
-                <Typography>Algo deu errado...</Typography>
-                <img src={NotFound} />
-                <Typography>A página que você requisitou não foi encontrada.</Typography>
-                <Link to={'/my-projects'}>
-                    <Button
-                        text="Voltar para Home"
-                        type="button"
-                        variant="primaryContained"
-                        onClick={() => { }}
-                    />
-                </Link>
+            <Grid
+                container
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid>
+                    <Typography
+                        variant="h2"
+                        color={theme.palette.neutral[70]}
+                    >
+                        Ooops...
+                    </Typography>
+                </Grid>
+                <Grid>
+                    <img src={NotFound} style={{
+                        width: '25rem',
+                        height: '25rem'
+                    }} />
+                </Grid>
+                <Grid mb="2rem">
+                    <Typography
+                        variant="h5"
+                        color={theme.palette.neutral[70]}
+                        textAlign="center"
+                    >A página que você requisitou não foi encontrada
+                    </Typography>
+                </Grid>
+                <Grid>
+                    <Link to={'/my-projects'}>
+                        <Button
+                            text="Voltar para Home"
+                            type="button"
+                            variant="primaryContained"
+                            onClick={() => { }}
+                        />
+                    </Link>
+                </Grid>
             </Grid>
-
         </Box>
     )
 }
