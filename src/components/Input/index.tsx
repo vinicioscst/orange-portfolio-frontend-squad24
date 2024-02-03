7665fa85f9d38319fcf7ea43bf2f68950f8fdf84
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-02-03 11:01:14 -0300

    fix: styles imports

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,6 @@
 import { HelpOutline, Visibility, VisibilityOff } from "@mui/icons-material";
-import { TextField, TextFieldVariants, Tooltip } from "@mui/material";
+import { TextField, TextFieldVariants, Tooltip, useTheme } from "@mui/material";
 import { ForwardedRef, forwardRef, useState } from "react";
-import theme from "../../style/globalStyle";
 import { FieldError } from "react-hook-form";
 interface InputProps {
   label: string;
@@ -14,9 +13,9 @@ interface InputProps {
 
 function Input(
   { label, variant = "outlined", type = "text", error, flexBasis, tooltip, ...rest }: InputProps, ref: ForwardedRef<HTMLInputElement>) {
-  
-  const [showPassword, setShowPassword] = useState(false);
 
+  const [showPassword, setShowPassword] = useState(false);
+  const theme = useTheme()
   function toggleShowPassword() {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   }
@@ -24,37 +23,37 @@ function Input(
   return (
     <>
       <TextField
-            type={showPassword ? "text" : type}
-            label={label}
-            variant={variant}
-            error={!!error}
-            helperText={error ? error.message : null}
-            ref={ref}
-            {...rest}
-            sx={{width: "100%", maxWidth: "45.1875rem", flexGrow: "1", flexBasis: flexBasis}}
-            InputProps={{
-              endAdornment: (
-                <>
-                  {type !== "password" ? null : showPassword ? (
-                    <VisibilityOff
-                      onClick={toggleShowPassword}
-                      sx={{color: theme.palette.action.active}}
-                    />
-                  ) : (
-                    <Visibility
-                      onClick={toggleShowPassword}
-                      sx={{color: theme.palette.action.active}}
-                    />
-                  )}
-                  {tooltip && (
-                    <Tooltip title={tooltip} arrow>
-                      <HelpOutline />
-                    </Tooltip>
-                  )}
-                </>
-              ),
-            }}
-          />
+        type={showPassword ? "text" : type}
+        label={label}
+        variant={variant}
+        error={!!error}
+        helperText={error ? error.message : null}
+        ref={ref}
+        {...rest}
+        sx={{ width: "100%", maxWidth: "45.1875rem", flexGrow: "1", flexBasis: flexBasis }}
+        InputProps={{
+          endAdornment: (
+            <>
+              {type !== "password" ? null : showPassword ? (
+                <VisibilityOff
+                  onClick={toggleShowPassword}
+                  sx={{ color: theme.palette.action.active }}
+                />
+              ) : (
+                <Visibility
+                  onClick={toggleShowPassword}
+                  sx={{ color: theme.palette.action.active }}
+                />
+              )}
+              {tooltip && (
+                <Tooltip title={tooltip} arrow>
+                  <HelpOutline />
+                </Tooltip>
+              )}
+            </>
+          ),
+        }}
+      />
     </>
   );
 }
